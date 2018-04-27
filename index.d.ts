12453daa1d4c1409ce725394823cb7ec4d493bd9
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2018-04-27 12:41:25 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,12 @@ import {Middleware, Dispatch} from "redux";
 
 
 export type ThunkAction<R, S> = (getState: () => S, dispatch: Dispatch<S>) => R;
+export type ActionSelector<R, S> = (getState: () => S) => R;
+
 
 declare module "redux" {
   export interface Dispatch<S>
-    { <R>(asyncAction: ThunkAction<R, S>): R }
+    { <R>(asyncAction: ThunkAction<R, S> | ActionSelector<R, S>): R }
 }
 
 
