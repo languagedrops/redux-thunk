c8b3aea538618b13bd3e5d1f8ef89af802ac77c1
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2018-04-28 16:30:00 +0100

    fix: correctly handle arrays of actions, etc

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const enhancedThunk = ({ dispatch, getState }) => (next) => (action) => {
+  return processAction(dispatch, getState, next, action)
+}
+
+const processAction = (dispatch, getState, next, action) => {
   if (typeof action === 'function') {
     const returnValue = action(getState, dispatch)
     if (returnValue && Array.isArray(returnValue)) {
-      returnValue.forEach(dispatch);
-    } else if (returnValue) {
-      dispatch(returnValue);
+      returnValue.forEach((returnedAction) => processAction(dispatch, getState, next, returnedAction))
+    } else if (returnValue && returnValue.type && typeof returnValue.type === 'string') {
+      return dispatch(returnValue);
+    } else if (returnValue && typeof returnValue.type === 'function') {
+      return processAction(dispatch, getState, next, returnValue);
     } else {
       return returnValue;
     }
+  } else {
+    return next(action)
   }
-
-  return next(action)
 }
+
 export default enhancedThunk
